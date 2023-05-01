47be1f9c7f0f2354e004d128fd38696f4bad5e11
oeuvars <www.anuragniall@gmail.com>
2023-05-01 14:08:51 +0530

    sec commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,6 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className='absolute inset-0 flex justify-center items-center phone:text-4xl lg:pb-0 phone:pb-48 lg:text-7xl font-playfair text-center opacity-80 text-amber-200'>Sinjari&apos;s Chatbot</main>
+    <main className='absolute inset-0 flex justify-center items-center phone:text-4xl lg:pb-0 phone:pb-72 lg:text-7xl font-playfair text-center opacity-80 text-amber-200'>Sinjari&apos;s Chatbot</main>
   )
 }
