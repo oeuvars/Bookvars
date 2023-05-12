46724db00276194990d5aae838e8b1a399c01f01
oeuvars <www.anuragniall@gmail.com>
2023-05-12 19:53:46 +0530

    Homepage Padding Fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,6 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className='absolute inset-0 flex justify-center items-center phone:text-4xl lg:pb-0 phone:pb-72 lg:text-7xl font-playfair text-center opacity-80 text-amber-200'>Anurag&apos;s Bookstore</main>
+    <main className='absolute inset-0 flex justify-center items-center phone:text-4xl lg:text-7xl font-playfair text-center opacity-80 text-amber-200'>Bookvars</main>
   )
 }
