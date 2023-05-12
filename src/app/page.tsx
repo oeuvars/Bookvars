8fa422184a6e2f3c9a18377df61062c78dc19f8b
oeuvars <www.anuragniall@gmail.com>
2023-05-12 20:30:21 +0530

    Padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,6 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className='absolute inset-0 flex justify-center items-start py-5 phone:text-4xl lg:text-7xl phone:font-outfit lg:font-playfair text-center text-amber-200'>Bookvars</main>
+    <main className='absolute inset-0 flex justify-center phone:items-start lg:items-center phone:py-5 lg:pt-0 phone:text-4xl lg:text-7xl phone:font-outfit lg:font-playfair text-center text-amber-200'>Bookvars</main>
   )
 }
