da10ea6798c4d37102702e87f97b4082dabe6ff0
oeuvars <www.anuragniall@gmail.com>
2023-05-12 20:18:42 +0530

    Homepage Padding Fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,6 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className='absolute inset-0 flex justify-center items-center phone:text-4xl lg:text-7xl font-playfair text-center opacity-80 text-amber-200'>Bookvars</main>
+    <main className='absolute inset-0 flex justify-center items-start py-5 phone:text-4xl lg:text-7xl phone:font-outfit lg:font-playfair text-center text-amber-200'>Bookvars</main>
   )
 }
--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -12,9 +12,9 @@ const Chat: FC = ({}) => {
     <Accordion
       type='single'
       collapsible
-      className='relative bg-white z-40 shadow'>
+      className='relative bg-white z-40 shadow left-8'>
         <AccordionItem value='item-1'>
-            <div className='fixed right-8 w-80 bottom-8 bg-white bg-opacity-95 border border-stone-50 rounded-md overflow-hidden'>
+            <div className='fixed lg:right-8 phone:w-10/12 lg:w-1/5 bottom-8 bg-white bg-opacity-95 border border-stone-50 rounded-md'>
               <div className='w-full h-full flex flex-col'>
                 <AccordionTrigger className='px-6 border-b border-zinc-300'>
                   <ChatHeader />
