733d995572773543eaf8b4d9d32310fb66eecb91
oeuvars <www.anuragniall@gmail.com>
2023-05-12 19:49:47 +0530

    Otfit Font Added

--- src/components/ChatMessages.tsx
+++ src/components/ChatMessages.tsx
@@ -16,7 +16,7 @@ const ChatMessages: FC<ChatMessagesProps> = ({className, ...props}) => {
 
   return (
   <div {...props} className={cn(
-    'flex flex-col-reverse gap-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch',
+    'flex flex-col-reverse gap-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch font-outfit',
     className
   )}>
     <div className='flex-1 flex-grow' />
